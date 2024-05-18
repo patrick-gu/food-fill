0827bee735b04b78858ef8c5e10907d92de1b69c
August R.M <sjsaug@gmail.com>
2024-05-18 18:13:15 -0400

    working model

--- recognization.py
+++ recognization.py
@@ -1,15 +1,20 @@
 from flask import Flask, request, jsonify
+from transformers import pipeline
 
 app = Flask(__name__)
 
 @app.route('/recognize', methods=['POST'])
 def recognize():
-    file = request.files['image']
+    file = request.json['url']
+    # print(file)
 
-    # ML
+    ml_pipeline = pipeline("image-classification", model="nateraw/food")
+    result = ml_pipeline(file)
+    # print(result)
+    result = max(result, key=lambda x: x['score'])
 
     return jsonify({
-        'food': 'Apple'
+        'food': result['label']
     })
 
 if __name__ == '__main__':
