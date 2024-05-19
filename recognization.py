3806a923c4e5ca74169cf4954e7871526170210b
August R.M <sjsaug@gmail.com>
2024-05-18 20:54:16 -0400

    remove comments

--- recognization.py
+++ recognization.py
@@ -6,16 +6,15 @@ app = Flask(__name__)
 @app.route('/recognize', methods=['POST'])
 def recognize():
     file = request.json['url']
-    # print(file)
 
     ml_pipeline = pipeline("image-classification", model="nateraw/food")
     result = ml_pipeline(file)
-    # print(result)
     result = max(result, key=lambda x: x['score'])
 
     return jsonify({
         'food': result['label']
     })
+
 
 if __name__ == '__main__':
     app.run()
